b5f4a39b1838eb8206a97b1edf14211ef3262b8d
CYBAI <cyb.ai.815@gmail.com>
2017-04-22 11:42:02 +0800

    Update yaml parser test

--- test/yaml-parser.test.js
+++ test/yaml-parser.test.js
@@ -1,13 +1,20 @@
 /* global suite, test */
 
 const assert = require('assert');
+const path = require('path');
 
-const { window } = require('vscode');
-const editor = window.activeTextEditor;
+const vscode = require('vscode');
 const { parseYaml } = require('../src/yaml-parser');
 
 suite("YAML Parser Tests", function () {
   test("Parse YAML function", function () {
-    assert.equal(false, parseYaml(editor));
+    vscode.workspace.openTextDocument(path.join(__dirname, './test.yml'))
+      .then(doc => vscode.window.showTextDocument(doc)
+        .then(editor => editor.edit(te => {
+          te.insert(doc.positionAt(Infinity), ' ');
+        })))
+      .then(editor => {
+        assert.equal(false, parseYaml(editor));
+      });
   });
 });
