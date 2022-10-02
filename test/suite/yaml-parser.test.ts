6b6456e980777191e70df745f8a3796e0de264ee
MrLuje <ogmniluje@gmail.com>
2022-10-02 20:46:04 +0000

    add test

--- test/suite/yaml-parser.test.ts
+++ test/suite/yaml-parser.test.ts
@@ -2,11 +2,53 @@ import * as assert from 'assert';
 import * as path from 'path';
 
 import * as vscode from 'vscode';
+import * as E from 'fp-ts/lib/Either';
+import { Error } from '../../src/constants';
 
 import { parseYaml } from '../../src/yaml-parser';
 
-suite('YAML Parser Tests', function() {
-    test('Parse YAML function', function() {
+async function moveCursorAt(x, y) {
+    await vscode.commands.executeCommand("cursorMove", {
+        to: "up",
+        by: "line",
+        value: 9000
+    });
+    await vscode.commands.executeCommand("cursorMove", {
+        to: "down",
+        by: "line",
+        value: y,
+    });
+    await vscode.commands.executeCommand("cursorMove", {
+        to: "right",
+        by: "character",
+        value: x,
+    });
+}
+
+async function getParsedResult(parsed: E.Either<Error, unknown>) {
+    let result = E.map(parsed =>
+        Object.keys(parsed).reduce((result, key) => {
+            result += !result ? parsed[key] : '.' + parsed[key];
+            return result;
+        }, '')
+    )(parsed);
+
+    return new Promise((ok, nok) => {
+        E.bimap<Error, void, string, void>(
+            err => {
+                nok(err);
+            },
+            res => {
+                ok(res);
+            }
+        )(result);
+    });
+}
+
+let relativeToCurrentFolder = file => path.join(__dirname.replace("/out", "/"), "..", file); 
+
+suite('YAML Parser Tests', function () {
+    test('Parse YAML function', function () {
         vscode.workspace
             .openTextDocument(path.join(__dirname, './test.yml'))
             .then(doc =>
@@ -21,5 +63,27 @@ suite('YAML Parser Tests', function() {
             .then(editor => {
                 assert.equal(false, parseYaml(editor));
             });
+    });
+
+    test('Read line 84 should equals to "canonical"', async () => {
+        let doc = await vscode.workspace.openTextDocument(relativeToCurrentFolder('./test.yml'))
+
+        let editor = await vscode.window.showTextDocument(doc);
+        await moveCursorAt(9, 83);
+
+        let parsed = parseYaml(editor);
+        let result = await getParsedResult(parsed);
+        assert.strictEqual(result, "float.canonical");
+    });
+
+    test('Read line 83 (without ---) should equals to "canonical"', async () => {
+        let doc = await vscode.workspace.openTextDocument(relativeToCurrentFolder('./test-headless.yml'))
+
+        let editor = await vscode.window.showTextDocument(doc);
+        await moveCursorAt(9, 82);
+
+        let parsed = parseYaml(editor);
+        let result = await getParsedResult(parsed);
+        assert.strictEqual(result, "float.canonical");
     });
 });
